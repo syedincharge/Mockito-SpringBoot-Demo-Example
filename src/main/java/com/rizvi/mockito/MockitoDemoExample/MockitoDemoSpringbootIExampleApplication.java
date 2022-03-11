032d07f800e9c26bff65f24f17bfec2de8c02482
syedincharge <rizvisyedba@gmail.com>
2022-03-11 01:35:45 -0600

    Initial commit

---
+++ src/main/java/com/rizvi/mockito/MockitoDemoExample/MockitoDemoSpringbootIExampleApplication.java
@@ -0,0 +1,13 @@
+package com.rizvi.mockito.MockitoDemoExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoDemoSpringbootIExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoDemoSpringbootIExampleApplication.class, args);
+	}
+
+}
